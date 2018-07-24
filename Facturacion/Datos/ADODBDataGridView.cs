6a72dcf77cd129597c6f5be16cffdd1a0b45628a
Jose Marfil <jose.marfil@itstuff.com.ar>
2018-07-23 21:09:22 -0300

    mejoras excepciones

--- Facturacion/Datos/ADODBDataGridView.cs
+++ Facturacion/Datos/ADODBDataGridView.cs
@@ -8,103 +8,113 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Facturacion.Excepciones;
 using System.Data.OleDb;
 
 namespace Facturacion
 {
-    public partial class ADODBDataGridView : System.Windows.Forms.DataGridView
+  public partial class ADODBDataGridView : System.Windows.Forms.DataGridView
+  {
+    #region privates
+    ADODB.Recordset _adoRs;
+    private DataTable ADODBRecordset_to_ADODataTable(ADODB.Recordset adoRs)
     {
-        #region privates
-        ADODB.Recordset _adoRs;
-        private DataTable ADODBRecordset_to_ADODataTable(ADODB.Recordset adoRs)
+      string tableName = "adoTable";
+      try
+      {
+        tableName = adoRs.Properties["Unique Table"].Value.ToString();
+      }
+      catch
+      {
+        // No existe el nombre de la tabla. 
+      }
+      tableName = (tableName == "") ? "adoTable" : tableName;
+      ADODB.Recordset dummyrs = adoRs.Clone(ADODB.LockTypeEnum.adLockReadOnly);
+      OleDbDataAdapter myDA = new OleDbDataAdapter();
+      DataSet myDS = new DataSet();
+      myDA.Fill(myDS, dummyrs, tableName);
+      dummyrs = null;
+      return myDS.Tables[0];
+    }
+
+    private void UpdateADODBRecordset_from_ADODataTable(DataTable inTable, ref ADODB.Recordset adoRs)
+    {
+      ADODB.Fields adoFields = adoRs.Fields;
+      System.Data.DataColumnCollection inColumns = inTable.Columns;
+      //Delete
+      if (adoRs.RecordCount > 0)
+      {
+        adoRs.MoveFirst();
+      }
+      while (!adoRs.EOF)
+      {
+        adoRs.Delete();
+        adoRs.MoveNext();
+
+      }
+      //Add
+      foreach (DataRow dr in inTable.Rows)
+      {
+        // Proceso las que no estan borradas
+        if (dr.RowState != DataRowState.Deleted)
         {
-            string tableName = "adoTable";
+          adoRs.AddNew(System.Reflection.Missing.Value,
+                          System.Reflection.Missing.Value);
+
+          for (int columnIndex = 0; columnIndex < inColumns.Count; columnIndex++)
+          {
+            ADODB.Field field = adoFields[columnIndex];
             try
             {
-                tableName = adoRs.Properties["Unique Table"].Value.ToString();
+              adoFields[columnIndex].Value = dr[columnIndex];
             }
-            catch
+            catch (Exception ex)
             {
-                // No existe el nombre de la tabla. 
+              string message = string.Format("Error al actualizar la columna {0}.{1}: {2}", inTable.TableName, field.Name, ex.Message);
+              throw new XTangoException(message);
             }
-            tableName = (tableName == "") ? "adoTable" : tableName;
-            ADODB.Recordset dummyrs = adoRs.Clone(ADODB.LockTypeEnum.adLockReadOnly);
-            OleDbDataAdapter myDA = new OleDbDataAdapter();
-            DataSet myDS = new DataSet();
-            myDA.Fill(myDS, dummyrs, tableName);
-            dummyrs = null;
-            return myDS.Tables[0];
+          }
         }
-
-        private void UpdateADODBRecordset_from_ADODataTable(DataTable inTable, ref ADODB.Recordset adoRs)
-        {
-            ADODB.Fields adoFields = adoRs.Fields;
-            System.Data.DataColumnCollection inColumns = inTable.Columns;
-            //Delete
-            if (adoRs.RecordCount > 0) { 
-                adoRs.MoveFirst(); 
-            }
-            while (!adoRs.EOF)
-            {
-                adoRs.Delete();
-                adoRs.MoveNext();
-
-            }
-            //Add
-            foreach (DataRow dr in inTable.Rows)
-            {
-                // Proceso las que no estan borradas
-                if (dr.RowState != DataRowState.Deleted)
-                {
-                    adoRs.AddNew(System.Reflection.Missing.Value,
-                                    System.Reflection.Missing.Value);
-
-                    for (int columnIndex = 0; columnIndex < inColumns.Count; columnIndex++)
-                    {
-                        adoFields[columnIndex].Value = dr[columnIndex];
-                    }
-                }
-            }
-        }
-
-        #endregion
-        public ADODBDataGridView()
-        {
-            InitializeComponent();
-        }
-
-        protected override void OnPaint(PaintEventArgs pe)
-        {
-            base.OnPaint(pe);
-        }
-        // Region Properties
-        /// <summary>
-        /// DatasSource como ADODB.Recordset
-        /// </summary>
-        public ADODB.Recordset ADODBDataSource
-        {
-            get
-            {
-                if (_adoRs != null)
-                {
-                    DataTable dt = (DataTable)this.DataSource;
-                    UpdateADODBRecordset_from_ADODataTable(dt, ref _adoRs);
-                    return _adoRs;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            set
-            {
-                this._adoRs = value;
-                if (this._adoRs != null)
-                {
-                    this.DataSource = ADODBRecordset_to_ADODataTable(this._adoRs);
-                }
-            }
-        }
+      }
     }
+
+    #endregion
+    public ADODBDataGridView()
+    {
+      InitializeComponent();
+    }
+
+    protected override void OnPaint(PaintEventArgs pe)
+    {
+      base.OnPaint(pe);
+    }
+    // Region Properties
+    /// <summary>
+    /// DatasSource como ADODB.Recordset
+    /// </summary>
+    public ADODB.Recordset ADODBDataSource
+    {
+      get
+      {
+        if (_adoRs != null)
+        {
+          DataTable dt = (DataTable)this.DataSource;
+          UpdateADODBRecordset_from_ADODataTable(dt, ref _adoRs);
+          return _adoRs;
+        }
+        else
+        {
+          return null;
+        }
+      }
+      set
+      {
+        this._adoRs = value;
+        if (this._adoRs != null)
+        {
+          this.DataSource = ADODBRecordset_to_ADODataTable(this._adoRs);
+        }
+      }
+    }
+  }
 }
--- Facturacion/Datos/xTangoAdapter.cs
+++ Facturacion/Datos/xTangoAdapter.cs
@@ -15,188 +15,199 @@ using XTANGO_GV;
 
 namespace Facturacion.Datos
 {
-    public static class xTangoProcesos {
-        public const int cosa = 1;
-    }
-    class xTangoAdapter
+  public static class xTangoProcesos
+  {
+    public const int cosa = 1;
+  }
+  class xTangoAdapter
+  {
+    HardLockServer.LogOnClass _tangoLogOn;
+
+    #region properties
+    public HardLockServer.LogOnClass tangoLogOn { get { return _tangoLogOn; } }
+    #endregion
+
+    private static xTangoAdapter singleton;
+
+    public static xTangoAdapter getInstance()
     {
-        HardLockServer.LogOnClass _tangoLogOn;
-
-        #region properties
-        public HardLockServer.LogOnClass tangoLogOn { get { return _tangoLogOn; } }
-        #endregion
-
-        private static xTangoAdapter singleton;
-
-        public static xTangoAdapter getInstance()
-        {
-            if (singleton == null)
-                singleton = new xTangoAdapter();
-            return singleton;
-        }
-
-        /// <summary>
-        /// Autentica el usuario en tango. 
-        /// </summary>
-        /// <param name="llaveTango"></param>
-        /// <param name="usuarioTango"></param>
-        /// <param name="passwordTango"></param>
-        /// <param name="nombreEmpresa"></param>
-        /// <returns>HardLockServer.LogOnClass con inormacion de autenticacion</returns>
-        public HardLockServer.LogOnClass TangoLogin(
-            string llaveTango,
-            string usuarioTango,
-            string passwordTango,
-            string nombreEmpresa)
-        {
-            string strError = "";
-            string appName = "";
-            try
-            {
-                appName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
-            }
-            catch { }
-
-            _tangoLogOn = new HardLockServer.LogOnClass();
-
-            bool logonOk = false;
-            try
-            {
-                logonOk = _tangoLogOn.LogServerUserEx(
-                                llaveTango,
-                                usuarioTango,
-                                passwordTango,
-                                nombreEmpresa,
-                                appName,
-                                ref strError
-                                );
-            }
-            catch (COMException ex) {
-                throw new Excepciones.XTangoException("Error en ingreso a tango: " + ex.Message);
-            }
-            if (!logonOk)
-            {
-                _tangoLogOn.DropInterface();
-                _tangoLogOn = null;
-                throw new Excepciones.XTangoException("Error en ingreso a tango: " + strError);
-            }
-            else
-                return _tangoLogOn;
-        }
-
-        public void TangoLogoff () {
-            _tangoLogOn.DropInterface();
-            _tangoLogOn = null;
-        }
+      if (singleton == null)
+        singleton = new xTangoAdapter();
+      return singleton;
     }
 
-    class xTangoFacturaData {
-        public ADODB.Recordset rsFacEncabezado { get; set; }
-        public ADODB.Recordset rsFacRenglones { get; set; }
-        public ADODB.Recordset rsFacImpuestos { get; set; }
-        public ADODB.Recordset rsFacCuotas { get; set; }
-        public ADODB.Recordset rsFonEncabezado { get; set; }
-        public ADODB.Recordset rsFonRenglones { get; set; }
-    }
-
-    class xTangoComprobantes : xTangoAdapter
+    /// <summary>
+    /// Autentica el usuario en tango. 
+    /// </summary>
+    /// <param name="llaveTango"></param>
+    /// <param name="usuarioTango"></param>
+    /// <param name="passwordTango"></param>
+    /// <param name="nombreEmpresa"></param>
+    /// <returns>HardLockServer.LogOnClass con inormacion de autenticacion</returns>
+    public HardLockServer.LogOnClass TangoLogin(
+        string llaveTango,
+        string usuarioTango,
+        string passwordTango,
+        string nombreEmpresa)
     {
-        /// <summary>
-        /// Crea las estructuras necesarias para la facturacion
-        /// </summary>
-        /// <returns></returns>
-        public bool makeEstructuras(out xTangoFacturaData data) {
-            // ------------------------------- INI TEST ------------------------------------//
-            XTANGO_GV.Comprobantes objComprob;
-            objComprob = (XTANGO_GV.Comprobantes) Activator.CreateInstance
-                (Type.GetTypeFromProgID("XTango_GV.Comprobantes"));
+      string strError = "";
+      string appName = "";
+      try
+      {
+        appName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+      }
+      catch { }
 
-            bool resultado = false;
-            data = new xTangoFacturaData();
+      _tangoLogOn = new HardLockServer.LogOnClass();
 
-            object Struct = null;
-            resultado = objComprob.Make_FacEncabezado(tangoLogOn, ref Struct);
-            if (!resultado)
-            {
-                throw new XTangoException("Fallo el metodo Make_FacEncabezado");
-            }
-            data.rsFacEncabezado = (ADODB.Recordset)Struct;
+      bool logonOk = false;
+      try
+      {
+        logonOk = _tangoLogOn.LogServerUserEx(
+                        llaveTango,
+                        usuarioTango,
+                        passwordTango,
+                        nombreEmpresa,
+                        appName,
+                        ref strError
+                        );
+      }
+      catch (COMException ex)
+      {
+        _tangoLogOn = null;
+        throw new Excepciones.XTangoException("Error en ingreso a tango: " + ex.Message);
+      }
+      // No se logueo, pero no hay excepcion
+      if (!logonOk)
+      {
+        _tangoLogOn = null;
+        throw new Excepciones.XTangoException("Error en ingreso a tango: " + strError);
+      }
+      else
+        return _tangoLogOn;
+    }
 
-            //Renglones
-            resultado = objComprob.Make_FacRenglones(tangoLogOn, ref Struct);
-            if (!resultado)
-            {
-                throw new XTangoException("Fallo el metodo Make_FacRenglones");
-            }
-            data.rsFacRenglones = (ADODB.Recordset)Struct;
+    public void TangoLogoff()
+    {
+      if (!(_tangoLogOn == null))
+      {
+        _tangoLogOn.DropInterface();
+        _tangoLogOn = null;
+      }
+    }
+  }
 
-            //Impuestos
-            resultado = objComprob.Make_FacImpuestos(tangoLogOn, ref Struct);
-            if (!resultado)
-            {
-                throw new XTangoException("Fallo el metodo Make_FacImpuestos");
-            }
-            data.rsFacImpuestos = (ADODB.Recordset)Struct;
+  class xTangoFacturaData
+  {
+    public ADODB.Recordset rsFacEncabezado { get; set; }
+    public ADODB.Recordset rsFacRenglones { get; set; }
+    public ADODB.Recordset rsFacImpuestos { get; set; }
+    public ADODB.Recordset rsFacCuotas { get; set; }
+    public ADODB.Recordset rsFonEncabezado { get; set; }
+    public ADODB.Recordset rsFonRenglones { get; set; }
+  }
 
-            //Cuotas
-            resultado = objComprob.Make_FacCuotas(tangoLogOn, ref Struct);
-            if (!resultado)
-            {
-                throw new XTangoException("Fallo el metodo Make_FacCuotas");
-            }
-            data.rsFacCuotas = (ADODB.Recordset)Struct;
+  class xTangoComprobantes : xTangoAdapter
+  {
+    /// <summary>
+    /// Crea las estructuras necesarias para la facturacion
+    /// </summary>
+    /// <returns></returns>
+    public bool makeEstructuras(out xTangoFacturaData data)
+    {
+      // ------------------------------- INI TEST ------------------------------------//
+      XTANGO_GV.Comprobantes objComprob;
+      objComprob = (XTANGO_GV.Comprobantes)Activator.CreateInstance
+          (Type.GetTypeFromProgID("XTango_GV.Comprobantes"));
 
-            //Fondos Encabezados
-            resultado = objComprob.Make_FondosEncabezado(tangoLogOn, ref Struct);
-            if (!resultado)
-            {
-                throw new XTangoException("Fallo el metodo Make_FondosEncabezado");
-            }
-            data.rsFonEncabezado = (ADODB.Recordset)Struct;
+      bool resultado = false;
+      data = new xTangoFacturaData();
 
-            //Fondos Renglones
-            resultado = objComprob.Make_FondosRenglones(tangoLogOn, ref Struct);
-            if (!resultado)
-            {
-                throw new XTangoException("Fallo el metodo Make_FondosRenglones");
-            }
-            data.rsFonRenglones = (ADODB.Recordset)Struct;
-            resultado = true;
-            return resultado;
+      object Struct = null;
+      resultado = objComprob.Make_FacEncabezado(tangoLogOn, ref Struct);
+      if (!resultado)
+      {
+        throw new XTangoException("Fallo el metodo Make_FacEncabezado");
+      }
+      data.rsFacEncabezado = (ADODB.Recordset)Struct;
+
+      //Renglones
+      resultado = objComprob.Make_FacRenglones(tangoLogOn, ref Struct);
+      if (!resultado)
+      {
+        throw new XTangoException("Fallo el metodo Make_FacRenglones");
+      }
+      data.rsFacRenglones = (ADODB.Recordset)Struct;
+
+      //Impuestos
+      resultado = objComprob.Make_FacImpuestos(tangoLogOn, ref Struct);
+      if (!resultado)
+      {
+        throw new XTangoException("Fallo el metodo Make_FacImpuestos");
+      }
+      data.rsFacImpuestos = (ADODB.Recordset)Struct;
+
+      //Cuotas
+      resultado = objComprob.Make_FacCuotas(tangoLogOn, ref Struct);
+      if (!resultado)
+      {
+        throw new XTangoException("Fallo el metodo Make_FacCuotas");
+      }
+      data.rsFacCuotas = (ADODB.Recordset)Struct;
+
+      //Fondos Encabezados
+      resultado = objComprob.Make_FondosEncabezado(tangoLogOn, ref Struct);
+      if (!resultado)
+      {
+        throw new XTangoException("Fallo el metodo Make_FondosEncabezado");
+      }
+      data.rsFonEncabezado = (ADODB.Recordset)Struct;
+
+      //Fondos Renglones
+      resultado = objComprob.Make_FondosRenglones(tangoLogOn, ref Struct);
+      if (!resultado)
+      {
+        throw new XTangoException("Fallo el metodo Make_FondosRenglones");
+      }
+      data.rsFonRenglones = (ADODB.Recordset)Struct;
+      resultado = true;
+      return resultado;
+    }
+    public bool addFacturas(bool defectos, bool contado, xTangoFacturaData data, out ADODB.Recordset adoReporte)
+    {
+      bool resultado = false;
+      // Clase para el alta de comprobantes.
+      XTANGO_GV.Comprobantes xComprobantes;
+      try
+      {
+        xComprobantes = (XTANGO_GV.Comprobantes)Activator.CreateInstance
+            (Type.GetTypeFromProgID("XTango_GV.Comprobantes"));
+
+        object reporte = null;
+
+        if (contado)
+        {
+          resultado = xComprobantes.Add_FacturaContado(tangoLogOn, data.rsFacEncabezado as object, data.rsFacRenglones as object, data.rsFacImpuestos as object, data.rsFonEncabezado as object, data.rsFonRenglones as object, defectos, ref reporte);
         }
-        public bool addFacturas(bool defectos, bool contado, xTangoFacturaData data, out ADODB.Recordset adoReporte) {
-            bool resultado = false;
-            // Clase para el alta de comprobantes.
-            XTANGO_GV.Comprobantes xComprobantes;
-            try
-            {                
-                xComprobantes = (XTANGO_GV.Comprobantes)Activator.CreateInstance
-                    (Type.GetTypeFromProgID("XTango_GV.Comprobantes"));
-
-                object reporte = null;
-
-                if (contado)
-                {
-                    resultado = xComprobantes.Add_FacturaContado(tangoLogOn, data.rsFacEncabezado as object, data.rsFacRenglones as object, data.rsFacImpuestos as object, data.rsFonEncabezado as object, data.rsFonRenglones as object, defectos, ref reporte);
-                }
-                else
-                {
-                    resultado = xComprobantes.Add_FacturaCtaCorriente(tangoLogOn, data.rsFacEncabezado as object, data.rsFacRenglones as object, data.rsFacImpuestos as object, data.rsFacCuotas as object, defectos, ref reporte);
-                }
-                // if (!resultado) { throw new XTangoException("Fallo al agregar facturas"); }
-
-                adoReporte = (ADODB.Recordset)reporte;
-            }
-            catch (Exception ex)
-            {
-                throw new Excepciones.XTangoException("Error al agregar facturas: " + ex.Message);
-            }
-            finally {
-                xComprobantes = null;
-            }
-
-            return resultado;
+        else
+        {
+          resultado = xComprobantes.Add_FacturaCtaCorriente(tangoLogOn, data.rsFacEncabezado as object, data.rsFacRenglones as object, data.rsFacImpuestos as object, data.rsFacCuotas as object, defectos, ref reporte);
         }
-    }        
+        // if (!resultado) { throw new XTangoException("Fallo al agregar facturas"); }
+
+        adoReporte = (ADODB.Recordset)reporte;
+      }
+      catch (Exception ex)
+      {
+        throw new Excepciones.XTangoException("Error al agregar facturas: " + ex.Message);
+      }
+      finally
+      {
+        xComprobantes = null;
+      }
+
+      return resultado;
+    }
+  }
 
 }
